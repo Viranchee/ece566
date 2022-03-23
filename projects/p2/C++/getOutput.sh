2fd14680242e60793a4e9d18e9e5c74ee268ce10
Viranchee Lotia <viranchee@outlook.com>
2022-03-23 05:16:14 -0400

    Update shellscript

--- projects/p2/C++/getOutput.sh
+++ projects/p2/C++/getOutput.sh
@@ -9,24 +9,24 @@ make EXTRA_SUFFIX=.M2RCSE CUSTOMFLAGS='-mem2reg' test
 
 echo "## Stats " > $prefix-Stats.txt
 echo "# Instructions" > $prefix-Stats.txt
-~/ece566/wolfbench/stats.py Instructions >> $prefix-Stats.txt
+~/ece566/wolfbench/fullstats.py Instructions >> $prefix-Stats.txt
 echo "# Loads" >> $prefix-Stats.txt
-~/ece566/wolfbench/stats.py Loads >> $prefix-Stats.txt
+~/ece566/wolfbench/fullstats.py Loads >> $prefix-Stats.txt
 echo "# Stores" >> $prefix-Stats.txt
-~/ece566/wolfbench/stats.py Stores >> $prefix-Stats.txt
+~/ece566/wolfbench/fullstats.py Stores >> $prefix-Stats.txt
 echo "# Functions" >> $prefix-Stats.txt
-~/ece566/wolfbench/stats.py Functions >> $prefix-Stats.txt
+~/ece566/wolfbench/fullstats.py Functions >> $prefix-Stats.txt
 echo "# CSEDead" >> $prefix-Stats.txt
-~/ece566/wolfbench/stats.py CSEDead >> $prefix-Stats.txt
+~/ece566/wolfbench/fullstats.py CSEDead >> $prefix-Stats.txt
 echo "# CSEElim" >> $prefix-Stats.txt
-~/ece566/wolfbench/stats.py CSEElim >> $prefix-Stats.txt
+~/ece566/wolfbench/fullstats.py CSEElim >> $prefix-Stats.txt
 echo "# CSESimplify" >> $prefix-Stats.txt
-~/ece566/wolfbench/stats.py CSESimplify >> $prefix-Stats.txt
+~/ece566/wolfbench/fullstats.py CSESimplify >> $prefix-Stats.txt
 echo "# CSELdElim" >> $prefix-Stats.txt
-~/ece566/wolfbench/stats.py CSELdElim >> $prefix-Stats.txt
+~/ece566/wolfbench/fullstats.py CSELdElim >> $prefix-Stats.txt
 echo "# CSEStore2Load" >> $prefix-Stats.txt
-~/ece566/wolfbench/stats.py CSEStore2Load >> $prefix-Stats.txt
+~/ece566/wolfbench/fullstats.py CSEStore2Load >> $prefix-Stats.txt
 echo "# CSEStElim" >> $prefix-Stats.txt
-~/ece566/wolfbench/stats.py CSEStElim >> $prefix-Stats.txt
+~/ece566/wolfbench/fullstats.py CSEStElim >> $prefix-Stats.txt
 echo "# Timing" >> $prefix-Stats.txt
 ~/ece566/wolfbench/timing.py >> $prefix-Stats.txt
