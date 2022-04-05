a373c598f2b5d64cdb0e421fdf53817f81028cef
Viranchee Lotia <viranchee@outlook.com>
2022-04-05 15:27:50 -0400

    Formatting

--- projects/p3/C++/p3.cpp
+++ projects/p3/C++/p3.cpp
@@ -1,28 +1,28 @@
+#include <algorithm>
 #include <fstream>
 #include <memory>
-#include <algorithm>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 #include "llvm-c/Core.h"
 
+#include "llvm/ADT/Statistic.h"
+#include "llvm/ADT/StringSet.h"
+#include "llvm/Analysis/InstructionSimplify.h"
+#include "llvm/Bitcode/BitcodeReader.h"
+#include "llvm/Bitcode/BitcodeWriter.h"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Verifier.h"
-#include "llvm/Bitcode/BitcodeWriter.h"
-#include "llvm/Bitcode/BitcodeReader.h"
-#include "llvm/ADT/StringSet.h"
-#include "llvm/ADT/Statistic.h"
 #include "llvm/IRReader/IRReader.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/ToolOutputFile.h"
-#include "llvm/Support/FileSystem.h"
-#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/LinkAllPasses.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/SourceMgr.h"
-#include "llvm/Analysis/InstructionSimplify.h"
+#include "llvm/Support/ToolOutputFile.h"
 
 using namespace llvm;
 
@@ -31,51 +31,47 @@ static void LoopInvariantCodeMotion(Module *);
 static void summarize(Module *M);
 static void print_csv_file(std::string outputfile);
 
-static cl::opt<std::string>
-        InputFilename(cl::Positional, cl::desc("<input bitcode>"), cl::Required, cl::init("-"));
+static cl::opt<std::string> InputFilename(cl::Positional,
+                                          cl::desc("<input bitcode>"),
+                                          cl::Required,
+                                          cl::init("-"));
 
-static cl::opt<std::string>
-        OutputFilename(cl::Positional, cl::desc("<output bitcode>"), cl::Required, cl::init("out.bc"));
-
-static cl::opt<bool>
-        Mem2Reg("mem2reg",
-                cl::desc("Perform memory to register promotion before LICM."),
-                cl::init(false));
-
-static cl::opt<bool>
-        CSE("cse",
-                cl::desc("Perform CSE before LICM."),
-                cl::init(false));
+static cl::opt<std::string> OutputFilename(cl::Positional,
+                                           cl::desc("<output bitcode>"),
+                                           cl::Required,
+                                           cl::init("out.bc"));
 
 static cl::opt<bool>
-        NoLICM("no-licm",
-              cl::desc("Do not perform LICM optimization."),
-              cl::init(false));
+    Mem2Reg("mem2reg",
+            cl::desc("Perform memory to register promotion before LICM."),
+            cl::init(false));
 
 static cl::opt<bool>
-        Verbose("verbose",
-                    cl::desc("Verbose stats."),
-                    cl::init(false));
+    CSE("cse", cl::desc("Perform CSE before LICM."), cl::init(false));
+
+static cl::opt<bool> NoLICM("no-licm",
+                            cl::desc("Do not perform LICM optimization."),
+                            cl::init(false));
 
 static cl::opt<bool>
-        NoCheck("no",
-                cl::desc("Do not check for valid IR."),
-                cl::init(false));
+    Verbose("verbose", cl::desc("Verbose stats."), cl::init(false));
+
+static cl::opt<bool>
+    NoCheck("no", cl::desc("Do not check for valid IR."), cl::init(false));
 
 int main(int argc, char **argv) {
     // Parse command line arguments
     cl::ParseCommandLineOptions(argc, argv, "llvm system compiler\n");
 
     // Handle creating output files and shutting down properly
-    llvm_shutdown_obj Y;  // Call llvm_shutdown() on exit.
+    llvm_shutdown_obj Y; // Call llvm_shutdown() on exit.
     LLVMContext Context;
 
     // LLVM idiom for constructing output file.
     std::unique_ptr<ToolOutputFile> Out;
     std::string ErrorInfo;
     std::error_code EC;
-    Out.reset(new ToolOutputFile(OutputFilename.c_str(), EC,
-                                 sys::fs::OF_None));
+    Out.reset(new ToolOutputFile(OutputFilename.c_str(), EC, sys::fs::OF_None));
 
     EnableStatistics();
 
@@ -85,20 +81,18 @@ int main(int argc, char **argv) {
     M = parseIRFile(InputFilename, Err, Context);
 
     // If errors, fail
-    if (M.get() == 0)
-    {
+    if (M.get() == 0) {
         Err.print(argv[0], errs());
         return 1;
     }
 
     // If requested, do some early optimizations
-    if (Mem2Reg || CSE)
-    {
+    if (Mem2Reg || CSE) {
         legacy::PassManager Passes;
-	if (Mem2Reg)
-	  Passes.add(createPromoteMemoryToRegisterPass());
-	if (CSE)
-	  Passes.add(createEarlyCSEPass());
+        if (Mem2Reg)
+            Passes.add(createPromoteMemoryToRegisterPass());
+        if (CSE)
+            Passes.add(createEarlyCSEPass());
         Passes.run(*M.get());
     }
 
@@ -114,8 +108,7 @@ int main(int argc, char **argv) {
         PrintStatistics(errs());
 
     // Verify integrity of Module, do this by default
-    if (!NoCheck)
-    {
+    if (!NoCheck) {
         legacy::PassManager Passes;
         Passes.add(createVerifierPass());
         Passes.run(*M.get());
@@ -129,7 +122,9 @@ int main(int argc, char **argv) {
 }
 
 static llvm::Statistic nFunctions = {"", "Functions", "number of functions"};
-static llvm::Statistic nInstructions = {"", "Instructions", "number of instructions"};
+static llvm::Statistic nInstructions = {"",
+                                        "Instructions",
+                                        "number of instructions"};
 static llvm::Statistic nLoads = {"", "Loads", "number of loads"};
 static llvm::Statistic nStores = {"", "Stores", "number of stores"};
 
@@ -153,8 +148,7 @@ static void summarize(Module *M) {
     }
 }
 
-static void print_csv_file(std::string outputfile)
-{
+static void print_csv_file(std::string outputfile) {
     std::ofstream stats(outputfile + ".stats");
     auto a = GetStatistics();
     for (auto p : a) {
@@ -165,13 +159,17 @@ static void print_csv_file(std::string outputfile)
 
 static llvm::Statistic NumLoops = {"", "NumLoops", "number of loops analyzed"};
 // add other stats
-static llvm::Statistic LICMBasic = {"", "LICMBasic", "basic loop invariant instructions"};
-static llvm::Statistic LICMLoadHoist = {"", "LICMLoadHoist", "loop invariant load instructions"};
-static llvm::Statistic LICMNoPreheader = {"", "LICMNoPreheader", "absence of preheader prevents optimization"};
-
+static llvm::Statistic LICMBasic = {"",
+                                    "LICMBasic",
+                                    "basic loop invariant instructions"};
+static llvm::Statistic LICMLoadHoist = {"",
+                                        "LICMLoadHoist",
+                                        "loop invariant load instructions"};
+static llvm::Statistic LICMNoPreheader = {
+    "",
+    "LICMNoPreheader",
+    "absence of preheader prevents optimization"};
 
 static void LoopInvariantCodeMotion(Module *) {
     // Implement this function
-  
 }
-
